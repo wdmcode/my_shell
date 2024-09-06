513583daf7c184f4f6734ed31191a7b6f8ecb26c
wangdaming <wangdaming@guanghe.tv>
2024-09-06 14:35:46 +0800

    feat: sh

---
+++ replace.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#
+# 该脚本用来替换 sql 中的某个值，替换的值来自 key.txt 文件中对的行的值
+#
+
+while read line; do
+    # echo $line
+    i=$((i + 1))
+    # echo $i
+    key1=$(sed -n "$((i * 2 - 1))p" key.txt)
+    key2=$(sed -n "$((i * 2))p" key.txt)
+    # echo $key1
+    # echo $key2
+
+    echo "$line" | sed "s#\('[^']*','[^']*','\)[^']*\('.*\)#\1${key2}\2#"
+    # echo $i
+done <sql.txt
